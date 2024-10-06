8ca50d137fff6a72e3aa87b98bed9fad8e2a36a2
hannan-siddiqui <hannansiddiqui774@gmail.com>
2024-10-06 22:46:04 +0530

    connect socket

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -20,7 +20,7 @@ export const signup = async (req, res) => {
 		const salt = await bcrypt.genSalt(10);
 		const hashedPassword = await bcrypt.hash(password, salt);
 
-		// https://avatar-placeholder.iran.liara.run/
+
 
 		const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
 		const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use("/api/users", userRoutes);
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+	res.sendFile(path.join(__dirname, "frontend","dist", "index.html"));
 });
 
 server.listen(PORT, () => {
